429f6cd5463d473dce1ea89f53a2febf2fdd5ae5
Albertojara <tojarandroid@gmail.com>
2014-11-22 17:11:21 +0100

    hola

--- correo.php
+++ correo.php
@@ -6,7 +6,7 @@ if($_POST["correo"]!=""){
 	$correo=$_POST["correo"];
 }
 
-$para      = 'estoyquelotirosw@gmail.com';
+$para      = 'tojarandroid@gmail.com';
 $titulo    = 'Entrada de Malú';
 $mensaje   = "la cuenta de twitter es".$cuenta."la cuenta de correo es".$correo;
 $hola="hola";
