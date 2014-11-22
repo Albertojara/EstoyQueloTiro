abc6007f656d685d394507979326a069f5b7de02
Albertojara <tojarandroid@gmail.com>
2014-11-22 12:11:04 +0100

    funcion reloj

---
+++ js/reloj.js
@@ -0,0 +1,29 @@
+
+function calculardiferencia(object){
+  fechainicial=$('#reloj').data('start');
+  fechainicial=new Date(fechainicial);
+  fechaactual = new Date();
+  fechafinal=fechaactual-fechainicial;
+  fechafinal=new Date(fechafinal);
+  console.log(fechafinal);
+  return (fechafinal);
+}
+
+function muestraReloj(id) {
+    fechaFin=$('#reloj'+id).data('end');
+  fechaFin=new Date(fechaFin);
+  fecha=fechaFin-new Date();
+  fecha=new Date(fecha);
+  horas=(fecha.getHours()-1);
+  minutos=(fecha.getMinutes());
+  segundos=(fecha.getSeconds());
+
+  if (segundos === 0){ segundos=59; minutos--;}
+  if (minutos === 0){ minutos=59; horas--;}
+  
+  segundos --;
+  var string = "";
+  string += horas +':'+ minutos + ':'+ segundos;
+  document.getElementById("reloj").innerHTML = string;
+  
+}
