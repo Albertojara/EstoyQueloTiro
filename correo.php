33f75e7f6354f9f29b85e9e178cc8038ef18b3f7
Albertojara <tojarandroid@gmail.com>
2014-11-22 17:19:05 +0100

    hola

--- correo.php
+++ correo.php
@@ -8,12 +8,12 @@ if($_POST["correo"]!=""){
 
 $para      = 'tojarandroid@gmail.com';
 $titulo    = 'Entrada de Malú';
-$mensaje   = "la cuenta de twitter es ".$cuenta." la cuenta de correo es".$correo;
+$mensaje   = "la cuenta de twitter es ".$cuenta." la cuenta de correo es ".$correo;
 $hola="hola";
 
-mail($para, $titulo,$mensaje);
+mail($para,$titulo,$mensaje);
 
 header("Location: http://estoyquelotiro.co");
 
 
-?>oy
+?>
